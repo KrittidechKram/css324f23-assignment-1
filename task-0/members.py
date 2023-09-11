df281c52543ec8b86e78dd44a28c0b79a844a829
Krittidech Paijitjinda <92147259+KrittidechKram@users.noreply.github.com>
2023-09-11 19:47:01 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Krittidech Paijitjinda", "sid": "6422781664"},
+    {"name": "Supakorn Vannathong", "sid": "6422782712"},
 ]
